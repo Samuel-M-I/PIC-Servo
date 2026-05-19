7b36022b3a70a184ebbe113bb4aab08bb824b70b
Samuel Marroquín Isaza <samuelmarroquinisaza@gmail.com>
2026-05-18 20:27:00 -0500

    Mi primer commit

---
+++ PWM.c
@@ -0,0 +1,13 @@
+/*
+ * File:   PWM.c
+ * Author: Usuario
+ *
+ * Created on May 18, 2026, 8:23 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: Usuario
+ *
+ * Created on May 18, 2026, 8:16 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
